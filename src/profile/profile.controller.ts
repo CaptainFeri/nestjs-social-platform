095faf986364f0f9069f667fed24edae4d1503f7
mohammad <mh.hsp1377@gmail.com>
2021-08-19 08:27:59 +0430

    profile functionality and injectable entity are generetaed

--- src/profile/profile.controller.ts
+++ src/profile/profile.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { User } from 'src/user/decorators/user.decorator';
 import { UserEntity } from 'src/user/user.entity';
+import { ProfileService } from './profile.service';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
 
 @Controller('profiles')
 export class ProfileController {
+    constructor(
+        private readonly profileService: ProfileService,
+    ) {}
+
     @Get(':username')
-    async getProfile(@User() currentUser: UserEntity,@Param('username') username: string) {
-        
+    async getProfile(@User() currentUser: UserEntity,@Param('username') username: string): Promise<ProfileResponseInterface> {
+        const profile  = await this.profileService.getProfile(currentUser.id,username);
+        return this.profileService.buildProfileResponse(profile);
     }
 }
